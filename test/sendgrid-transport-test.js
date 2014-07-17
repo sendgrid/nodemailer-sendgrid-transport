1e9066df5165b383e07e70aa3d491e1adfebcbdf
Eddie Zaneski <eddiezane@gmail.com>
2014-07-17 00:43:12 -0400

    Add mocha and chai

---
+++ test/sendgrid-transport-test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('sendgrid-transport', function() {
+  it('should take an api_user and api_key', function() {
+  });
+
+  it('should have a name and version', function() {
+  });
+});
